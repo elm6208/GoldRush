0e0b8bbeb4bd880baf4452bc5ce2b1f19179a4df
MAIN\jas3593 <jas3593@rit.edu>
2017-02-10 14:49:02 -0500

    Basic movement and methods implemented. Correct following and collision handling still needs to be implemented.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public GameObject currTarget;
+    public float maxSpeed;
+    public int hp;
+    Vector3 move;
+    float maxDist;
+
+
+	// Use this for initialization
+	void Start () {
+        hp = 10;
+        maxSpeed = 0.1f;
+        maxDist = 0;
+        move = new Vector3(maxSpeed, 0, 0);
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(move);
+        maxDist += move.magnitude;
+		
+	}
+
+    void onTriggerEnter(Collider collision){
+        GameObject thing = collision.gameObject;
+
+        if(thing.tag == "target"){
+            //updateTarget(thing.nextTarg);
+        }
+
+
+
+        //collision handling
+    }
+
+    public void takeDamage(int dmg){
+        hp -= dmg;
+
+        if(hp <= 0){
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void moveToTarget(){
+
+    }
+
+    void updateTarget(GameObject newTarg){
+        currTarget = newTarg;
+        if(move.x == 0){
+            move.x = maxSpeed;
+            move.y = 0;
+        }else{
+            move.y = maxSpeed;
+            move.x = 0;
+        }
+    }
+
+    public float getMaxDist(){
+        return maxDist;
+    }
+}
