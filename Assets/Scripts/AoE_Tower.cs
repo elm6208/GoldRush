24d485f84cfa8c6af58c8a2af7f0f2f0552a2c48
MAIN\epn8794 <epn8794@g.rit.edu>
2017-02-22 15:48:59 -0500

    Added AOE tower

---
+++ Assets/Scripts/AoE_Tower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AoE_Tower : Tower {
+
+    public Dynamite dynamitePrefab;
+	// Use this for initialization
+	void Start () {
+        rangeCollider = GetComponent<SphereCollider>();
+        
+        rangeCollider.radius = range;
+        enemies = new List<GameObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Enemy target = Aim();
+
+        if (target != null && fireCooldown <= 0)
+        {
+            Aoe_Attack(target);
+        }
+        else {
+            fireCooldown -= Time.deltaTime;
+        }
+    }
+
+    void Aoe_Attack(Enemy target)
+    {
+        print("Lobbing bomb");
+        fireCooldown = fireRate;
+        Dynamite atkr = (Dynamite)Instantiate(dynamitePrefab, transform.position, transform.rotation);
+        atkr.SetTarget(target);
+    }
+}
